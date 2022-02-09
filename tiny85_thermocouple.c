3ded96fce09bb419a224500ec062f45523be9a8c
lukaszrychlowski <rychlowski@agh.edu.pl>
2022-02-09 23:49:10 +0100

    i2c added

--- tiny85_thermocouple.c
+++ tiny85_thermocouple.c
@@ -1,9 +1,13 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/pgmspace.h>
+
 //pins
 #define sda PB0
 #define scl PB2
 
-//macro
-#define sda_high DDRB &= ~(1<<sda); //release sda, pulled high
+//macros
+#define sda_high DDRB &= ~(1<<sda); //release sda, pulled high by oled chip
 #define sda_low DDRB |= (1<<sda); //pulled low by tiny
 #define scl_high DDRB &= ~(1<<sda);
 #define scl_low DDRB |= (1<<scl);
@@ -32,10 +36,13 @@ void i2c_stop(){
 void i2c_write(uint8_t dat){
     sda_low();
     for (uint8_t i=8;i;i--){
-        if ((dat << i) & 0x80) sda_high();
+        if ((dat << i) & 0b10000000) sda_high(); //check if msb == 1, if so sda high
         scl_high();
         dat = dat << 1;
         scl_low();
     }
-
+    sda_high();
+    scl_high();
+    asm("nop");
+    scl_low();
 }
