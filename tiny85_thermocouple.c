9ab1d7ad5b4dea25e7fcc6f1176e79c9f197f195
lukaszrychlowski <rychlowski@agh.edu.pl>
2022-02-13 01:30:38 +0100

    foundations for displaying stuff

--- tiny85_thermocouple.c
+++ tiny85_thermocouple.c
@@ -1,5 +1,3 @@
-#define __DELAY_BACKWARD_COMPATIBLE__ 1
-
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/pgmspace.h>
@@ -24,7 +22,7 @@ void i2c_init(void) {
 void i2c_write(uint8_t dat){
     for (uint8_t i = 8; i; i--){
         sda_low();
-        if (dat & 0x80) sda_high(); //check if msb == 1, if so sda high
+        if (dat & 0b10000000) sda_high(); //check if msb == 1, if so sda high
         scl_high();
         dat = dat << 1;
         scl_low();
@@ -66,7 +64,7 @@ const uint8_t oled_init_commands[] PROGMEM = {                  //init cmds stor
     0xAE,             // oled off
     0x20, 0x00,       // set horizontal memory addressing mode, POR (whatever it is) = 0x00
     0xA8, 0x7F,       // set multiplex 0x7F for 128x128px display (0x3F displays half a screen (64px), 0x1F displays 32px)
-    0xA5,             // every pixel on display on regardles of the display data RAM
+    0xA4,             // A4 - normal, A5 - every pixel on display on regardles of the display data RAM
     0xAF,             // switch on OLED
 };
 
@@ -78,6 +76,34 @@ void oled_init(void){
     i2c_stop();
 }
 
+void oled_set_cursor(uint8_t x){
+    i2c_start(oled_addr);
+    i2c_write(oled_send_cmd);
+    i2c_write(x & 0b00001111);           // set lower col adress (last for bits of 0b00000000, 00H - 0FH (0b00001111))
+    i2c_write(0b00010000 | (x >> 4));    // set higher col adress (last 3 bits of 0b00010000, 10H - 17H)
+    i2c_write(0xB0);              // set page address (B0H - BFH)
+    i2c_stop();
+}
+
+void oled_pixel_on(void){
+    i2c_start(oled_addr);
+    i2c_write(oled_send_data);
+    i2c_write(0xFF);
+    i2c_stop();
+    
+}
+
+void oled_pixel_off(void){
+    i2c_start(oled_addr);
+    i2c_write(oled_send_data);
+    i2c_write(0x00);
+    i2c_stop();
+}
+
 int main(void){
     oled_init();
+    oled_set_cursor();
+    oled_pixel_off();
+    oled_pixel_on();
 }
+
